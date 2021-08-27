cfbd4648d87c90adb543f13eea1387d8cda54875
Iva <84078385+Mistymos3@users.noreply.github.com>
2021-08-26 17:34:43 -0700

    added logic.js page and questions.js page and scores.js page, fixed links to css and js pages, added some questions

---
+++ Develop/js/questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {   
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text MarkUp Language", "Hyper text markdown language"],
+        answers: ["Hyper Text MarkUp Language"]
+    },
+
+    {   
+        title: "",
+        choices: [""],
+        answers: [""]
+    },
+    {   
+        title: "Which of the following are JSON syntax rules?",
+        choices: ["data is in name/value pair", "Data is seperated by commas", "Curly braces hold objects", "Square brackets hold arrays", "All of the above"],
+        answers: ["All of the above"]
+    },
+
+    {   
+        title: "Is the JSON parse() method is used to convert the string from a web server to a JavaScript object?",
+        choices: ["True", "false"],
+        answers: ["True"]
+    },  
+    
+    {   
+        title: "The JSON stringify() method is used to convert an object into a string when sending data to a web server",
+        choices: ["True", "False"],
+        answers: ["True"]
+    }
+];
